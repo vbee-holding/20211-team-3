5e647384063aee42357ea2fe68333c6950dbe32f
2uan <quan.qvv@gmail.com>
2022-01-29 22:32:07 +0700

    crawl and push to db, clean code

--- crawler/crawl_model.js
+++ crawler/crawl_model.js
@@ -1,29 +1,28 @@
 import Crawler from "crawler"
 
 class BaseModel{
+
     constructor(res){
         this.res = res
-        this.host_name = null
+        this.hostName = null
         this.articles = []
         this.init()
     }
 
-    init(){
+    init(){}
 
-    }
-
-    get_host_name_url(){
-        return "https://" + this.host_name + "/"
+    getHostNameUrl(){
+        return "https://" + this.hostName + "/"
     }
 
     canParse(){
-        return this.res.request.uri.hostname == this.host_name
+        return this.res.request.uri.hostname == this.hostName
     }
 
     normalizeTime(article){
-        let day = article.release_time.split(" ")[0].split("/").reverse().join("/")
-        let time = article.release_time.split(" ")[1]
-        article.release_time = day + " " + time
+        const day = article.dateCreate.split(" ")[0].split("/").reverse().join("/")
+        const time = article.dateCreate.split(" ")[1]
+        article.dateCreate = day + " " + time
     }
 
     parse(){
@@ -34,28 +33,29 @@ class BaseModel{
 class ZingNewsModel extends BaseModel{
 
     init(){
-        this.host_name = "zingnews.vn"
+        this.hostName = "zingnews.vn"
     }
 
     parse(){
-        let $ = this.res.$
-        let self = this
+        const $ = this.res.$
+        const self = this
         $("article").each((index, element)=>{
-            let element_query =  $(element)
-            let article = {
+            const element_query =  $(element)
+            const article = {
                 title: element_query.find('.article-title').text().trim(),
-                link: self.get_host_name_url() + element_query.find('.article-thumbnail a')
+                link: self.getHostNameUrl() + element_query.find('.article-thumbnail a')
                     .attr("href"),
                 thumbnail: element_query.find('.article-thumbnail a img').attr("data-src"),
                 sapo: element_query.find(".article-summary").text().trim(),
                 category: element_query.find(".category").text().trim() || element_query.find(".article-category").text().trim(),
-                source: self.host_name,
-                release_time: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
+                source: self.hostName,
+                dateCreate: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
             }
-            article.release_time = article.release_time.split(" ").reverse().join(" ")
+            article.dateCreate = article.dateCreate.split(" ").reverse().join(" ")
             this.normalizeTime(article)
             this.articles.push(article)
         })
+
         return this.articles
     }
 }
@@ -63,138 +63,139 @@ class ZingNewsModel extends BaseModel{
 class SuckhoedoisongModel extends BaseModel{
 
     init(){
-        this.host_name = "suckhoedoisong.vn"
+        this.hostName = "suckhoedoisong.vn"
     }
 
     parse(){
-        let $ = this.res.$
-        let self = this
+        const $ = this.res.$
+        const self = this
         $(".box-category-item").each((index, element)=>{
-            let element_query =  $(element)
-            let article = {
+            const element_query =  $(element)
+            const article = {
                 title: element_query.find('a[data-type="title"]').text().trim(),
-                link: self.get_host_name_url() + element_query.find('a[data-type="title"]')
+                link: self.getHostNameUrl() + element_query.find('a[data-type="title"]')
                     .attr("href"),
                 thumbnail: element_query.find('img[data-type="avatar"]').attr("src"),
                 sapo: element_query.find('p.box-category-sapo').text().trim(),
                 category: element_query.find(".box-category-category").attr("title"),
-                source: self.host_name,
-                release_time: element_query.find(".box-category-time.time-ago").text()
+                source: self.hostName,
+                dateCreate: element_query.find(".box-category-time.time-ago").text()
             }
             this.normalizeTime(article)
             this.articles.push(article)
         })
+
         return this.articles
     }
 }
 
 class BaoTinTucModel extends BaseModel{
     init(){
-        this.host_name = "baotintuc.vn"
+        this.hostName = "baotintuc.vn"
     }
 
     parse(){
-        let $ = this.res.$
-        let self = this
+        const $ = this.res.$
+        const self = this
         $(".ccl-item").each((index, element)=>{
-            let element_query =  $(element)
-            let article = {
+            const element_query =  $(element)
+            const article = {
                 title: element_query.find('.item_title').text().trim(),
-                link: self.get_host_name_url() + element_query.find('a[data-type="title"]')
+                link: self.getHostNameUrl() + element_query.find('a[data-type="title"]')
                     .attr("href"),
                 thumbnail: element_query.find('img').attr("src"),
                 sapo: element_query.find('.item_title').text().trim(),
                 category: element_query.find(".item_info .item_cat").text().trim(),
-                source: self.host_name,
-                release_time: element_query.find(".box-category-time.time-ago").text()
+                source: self.hostName,
+                dateCreate: element_query.find(".box-category-time.time-ago").text()
             }
             this.normalizeTime(article)
             this.articles.push(article)
         })
+
         return this.articles
     }
 }
 
 class TienphongModel extends BaseModel{
     init(){
-        this.host_name = "tienphong.vn"
+        this.hostName = "tienphong.vn"
     }
 
     parse(){
-        let $ = this.res.$
-        let self = this
+        const $ = this.res.$
+        const self = this
         $("article").each((index, element)=>{
-            let element_query =  $(element)
-            let article = {
+            const element_query =  $(element)
+            const article = {
                 title: element_query.find('a[title]').text().trim(),
                 link: element_query.find('a').attr("href"),
                 thumbnail: element_query.find('img').attr("data-src"),
                 sapo: element_query.find(".story__summary").text().trim(),
                 category: element_query.find(".story__meta a").text().trim() || element_query.find(".article-category").text().trim(),
-                source: self.host_name,
-                release_time: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
+                source: self.hostName,
+                dateCreate: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
             }
-            article.release_time = article.release_time.split(" ").reverse().join(" ")
+            article.dateCreate = article.dateCreate.split(" ").reverse().join(" ")
             this.normalizeTime(article)
             this.articles.push(article)
         })
+
         return this.articles
-        
     }
 }
 
 class VnExpressModel extends BaseModel{
     init(){
-        this.host_name = "vnexpress.net"
+        this.hostName = "vnexpress.net"
     }
 
     parse(){
-        let $ = this.res.$
-        let self = this
+        const $ = this.res.$
+        const self = this
         $(".item-news.item-news-common").each((index, element)=>{
-            let element_query =  $(element)
-            let article = {
+            const element_query =  $(element)
+            const article = {
                 title: element_query.find('h3 a[title]').text().trim(),
                 link: element_query.find('a').attr("href"),
                 thumbnail: element_query.find('img').attr("data-src"),
                 sapo: element_query.find(".description a").text().trim(),
                 category: element_query.find(".story__meta a").text().trim() || element_query.find(".article-category").text().trim(),
-                source: self.host_name,
-                release_time: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
+                source: self.hostName,
+                dateCreate: (element_query.find(".time").text() + " " + element_query.find(".date").text()).trim()
             }
-            article.release_time = article.release_time.split(" ").reverse().join(" ")
+            article.dateCreate = article.dateCreate.split(" ").reverse().join(" ")
             this.normalizeTime(article)
             this.articles.push(article)
-            
         })
+        
         return this.articles
     }
 }
 
-var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error)
-        }else{
-            let articles = []
-            // console.log(res)
-            // console.log(res.request.uri.hostname)
-            console.log(new VnExpressModel(res).parse())
+function test() {
+    var crawler = new Crawler({
+        maxConnections : 10,
+        // This will be called for each crawled page
+        callback : function (error, res, done) {
+            if(error){
+                console.log(error)
+            }else{
+                console.log(new VnExpressModel(res).parse())
+            }
+            done();
         }
-        done();
-    }
-});
-// Queue just one URL, with default callback
-// c.queue('https://zingnews.vn/');
-// c.queue('https://suckhoedoisong.vn/');
-// c.queue("https://baotintuc.vn/")
-// c.queue("https://tienphong.vn/")
-// c.queue('https://vietnamnet.vn/')
-// c.queue('https://tienphong.vn/')
-// c.queue('https://nhandan.vn/')
-// c.queue('https://vnexpress.net/')
+    });    
+    // Queue just one URL, with default callback
+    // crawler.queue('https://zingnews.vn/');
+    // crawler.queue('https://suckhoedoisong.vn/');
+    // crawler.queue("https://baotintuc.vn/")
+    // crawler.queue("https://tienphong.vn/")
+    // crawler.queue('https://vietnamnet.vn/')
+    // crawler.queue('https://tienphong.vn/')
+    // crawler.queue('https://nhandan.vn/')
+    // crawler.queue('https://vnexpress.net/')
+}
 
-var crawlerModelClasses = [TienphongModel, ZingNewsModel, BaoTinTucModel, SuckhoedoisongModel, VnExpressModel]
+const crawlerModelClasses = [TienphongModel, ZingNewsModel, BaoTinTucModel, SuckhoedoisongModel, VnExpressModel]
 export {crawlerModelClasses}
--- crawler/main.js
+++ crawler/main.js
@@ -1,11 +1,53 @@
 import { crawlerModelClasses } from "./crawl_model.js";
 import Crawler from "crawler";
+import Service from "./service.js";
+import Utils from "./utils.js";
+import moment from "moment"
+import {CronJob} from "cron"
+
+const service = new Service()
+let allCategories = service.getAllCategories()
+const adminCrawler = "61f2466507c5fb78061d2c74"
+
+async function putCrawledArticle(article){
+    try{
+        let articleMapped = {
+            title: article.title,
+            content: "",
+            tag: [],
+            status: "published",
+            cateNews: getCategory(article.category),
+            createdBy: {
+                _id: adminCrawler
+            },
+            articlePicture: article.thumbnail,
+            originalLink: article.link,
+            sapo: article.sapo,
+            source: article.source,
+            dateCreate: moment(Date.parse(article.dateCreate)).format('YYYY-MM-DD HH:mm:ss Z')
+        }
+        if(!Object.values(articleMapped).map(value => value === undefined).includes(true))
+            console.log(await (await new Service().putNews(articleMapped)).data.message)
+        else
+            console.log("Has field undefined")
+    }catch(err){
+        console.log("Error put: " + err)
+    }
+}
+
+function getCategory(categoryRaw) {
+    for(let cate of allCategories){
+        if(Utils.stringToSlug(categoryRaw) == Utils.stringToSlug(cate.name)){
+            return cate
+        }
+    }
+}
 
 class ArticalCrawler{
     constructor(){
         this.result = []
         let self = this
-        this.c = new Crawler({
+        this.crawler = new Crawler({
             maxConnections : 10,
             // This will be called for each crawled page
             callback : function (error, res, done) {
@@ -16,7 +58,10 @@ class ArticalCrawler{
                         let model = new modelClass(res)
                         if(model.canParse()){
                             Array.prototype.push.apply(self.result, model.parse(model))
-                            console.log(self.result.length)
+                            self.result = self.result.slice(0, 10)
+                            for(let artical of self.result){
+                                putCrawledArticle(artical)
+                            }
                         }
                     }
                 }
@@ -29,21 +74,30 @@ class ArticalCrawler{
         this.crawler.queue()
     }
 
-
     crawl(){
-        let c = this.c
         // Queue just one URL, with default callback
-        c.queue('https://zingnews.vn/');
-        c.queue('https://suckhoedoisong.vn/');
-        c.queue("https://baotintuc.vn/")
-        // c.queue("https://tienphong.vn/")
-        // c.queue('https://vietnamnet.vn/')
-        // c.queue('https://tienphong.vn/')
-        // c.queue('https://nhandan.vn/')
-        // c.queue('https://vnexpress.net/')
+        this.crawler.queue('https://zingnews.vn/');
+        // this.crawler.queue('https://suckhoedoisong.vn/');
+        // this.crawler.queue("https://baotintuc.vn/")
+        // this.crawler.queue("https://tienphong.vn/")
+        // this.crawler.queue('https://vietnamnet.vn/')
+        // this.crawler.queue('https://tienphong.vn/')
+        // this.crawler.queue('https://nhandan.vn/')
+        // this.crawler.queue('https://vnexpress.net/')
     }
 }
 
-let crawler = new ArticalCrawler()
-crawler.crawl()
-console.log(crawler.result)
+var job = new CronJob('*/1 * * * *', function() {
+    service.getAllCategories().then(async (categories)=>{
+        try{
+            console.log('Start: ' + new Date());
+            allCategories = categories
+            let crawler = new ArticalCrawler()
+            crawler.crawl()
+        }catch(error){
+            console.log(err)
+        }  
+    })
+
+  }, null, true);
+  job.start();
