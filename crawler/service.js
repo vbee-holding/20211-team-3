c811d9b4b94e9ee9820f44339e5a446377a6775e
2uan <quan.qvv@gmail.com>
2022-01-29 22:32:07 +0700

    crawl and push to db, clean code

---
+++ crawler/service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+class Service {
+
+    getAPI(entityName){
+        const api = axios.create({
+            baseURL: 'http://localhost:3000/' + entityName
+        });
+        
+        return api
+    }
+
+    async putNews(newsData) {
+        return await this.getAPI("news").post("/crawled_news", newsData)
+    }
+
+    async getAllCategories(){
+        let res = await this.getAPI("cateNews").get("/")
+
+        return res.data.data
+    }
+}
+
+export default Service;
