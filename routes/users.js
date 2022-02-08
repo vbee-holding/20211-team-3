cc6e49c7987e03f7685daae180a936179a755b47
20187288-anhddt <73302743+20187288-anhddt@users.noreply.github.com>
2022-02-08 21:28:54 +0700

    Update users.js
    update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,12 @@ var router = express.Router();
 const UserModel = require("../models/User");
 const users = require("../controllers/users");
 
-router.get('/', users.users);
-router.get('/channels', users.channels);
-router.post('/locked/:id', users.locked);
-router.put('/updateRole/:id', users.updateRole);
-router.delete("/:id", users.deleted);
-router.get('/name/:id', users.get_user);
+router.get('/users', users.getUsers);
+router.get('/channels', users.getChannels);
+router.post('/lock/:id', users.lockUser);
+router.put("/update/:id", users.updateName, users.updateEmail,users.updatePassword, users.uploadAvatar);
+router.delete("/:id", users.deleteUser);
+router.put('/role/:id', users.updateRole)
+router.get('/users/:id', users.getUser);
 
 module.exports = router;
