ce5d2f2cdf81f40aa09e8436c9f8bb1e6d0fe60d
20187288-anhddt <73302743+20187288-anhddt@users.noreply.github.com>
2022-02-08 21:34:43 +0700

    Update categories.js
    update route

--- routes/categories.js
+++ routes/categories.js
@@ -2,12 +2,12 @@ var express = require("express");
 var router = express.Router();
 const categories = require("../controllers/categories");
 
-router.get('/categories', categories.get_categories)
-router.get('/categories/trash', categories.get_trash)
-router.get('/categories/:_id', categories.get_idCate)
-router.post('/categories', categories.post_categories)
-router.put("/categories/trash/:_id", categories.move_to_trash)
-router.put("/restore/:_id", categories.restoreFromTrash)
-router.delete("/categories/:id", categories.delete)
+router.get('/categories', categories.getCategories)
+router.get('/categories/trash', categories.getTrash)
+router.get('/categories/:id', categories.getIdCate)
+router.post('/categories', categories.postCategories)
+router.put("/categories/trash/:id", categories.moveToTrash)
+router.put("/restore/:id", categories.restoreFromTrash)
+router.delete("/categories/:id", categories.deleteCate)
 
 module.exports = router;
