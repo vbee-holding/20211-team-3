6ae18b7702a04afc41cbfd29710be0412c8824f5
20187288-anhddt <73302743+20187288-anhddt@users.noreply.github.com>
2022-02-12 15:19:55 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,6 @@ const users = require("../controllers/users");
 router.get('/users', users.getUsers);
 router.get('/channels', users.getChannels);
 router.post('/lock/:id', users.lockUser);
-router.put("/update/:id", users.updateName, users.updateEmail,users.updatePassword, users.uploadAvatar);
 router.delete("/:id", users.deleteUser);
 router.put('/role/:id', users.updateRole)
 router.get('/users/:id', users.getUser);
