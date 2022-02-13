eb3a7ed707c317de5d32fe16f5910ea7136968db
20187288-anhddt <73302743+20187288-anhddt@users.noreply.github.com>
2022-02-13 17:06:24 +0700

    edit news

--- routes/news.js
+++ routes/news.js
@@ -6,32 +6,16 @@ const news = require("../controllers/news")
 const app = express();
 app.use(fileUpload());
 
-// get news entertainment
-router.get("/newsEntertainments", news.news_entertainments);
-router.get("/newsReels", news.news_Reel);
+router.get("/newsReels", news.newsReel);
 router.get("/q", news.search);
-router.get("/latestNews", news.latenews);
-router.get("/other", news.other);
+router.get("/latestNews", news.lateNews);
+router.get("/other", news.newsOther);
 router.get("/newsForYou", news.newsForYou);
-router.get("/published", news.news_published);
-router.get("/:id", news.news_by_id_user);
-router.get("/new/:id", news.news_edit);
-router.get("/trash/:id", news.news_isdelete_true);
-router.get("/categories/:id", news.news_of_categories);
-router.get("/users/:id", news.news_of_users_channel);
-router.get("/similar/:id", news.similar);
-router.get("/:_idNews", news.get_news);
-router.get("/details/:_idNews", news.details);
-router.post("/", news.add_news);
-router.post("/upload", news.add_news_upload_picture);
-router.put("/:_id", news.edit_news);
-router.put("/views/:_id", news.views);
-router.put("/giveUpDraft/:_id", news.giveUpDraft);
-router.put("/saveDraft/:_id", news.saveDraft);
-router.put("/trash/:_id", news.move_new_to_trash);
-router.delete("/:_id", news.delete);
-router.get("/favorite", news.favorite);
+router.get("/published", news.newsPublished);
+router.get("/categories/:id", news.newsOfCategories);
+router.get("/similar/:id", news.newsSimilar);
+router.get("/:idNews", news.getNews);
+router.get("/details/:idNews", news.getNewsDetails);
 router.get("/bestNews", news.bestNews);
-router.get("/", news.news_false);
 
 module.exports = router;
